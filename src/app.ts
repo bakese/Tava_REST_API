f17745314b000df5bf587b694a397109641870d0
Seth Baker <sethlbaker09@gmail.com>
2024-11-16 14:28:26 -0700

    fixed port casing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port: number = Number(process.env.port);
+const port: number = Number(process.env.PORT);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
